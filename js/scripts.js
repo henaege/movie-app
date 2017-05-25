750bf1d82e9ed92894a9010c6a7474f290914d85
henaege <henaege@gmail.com>
2017-05-25 12:01:56 -0400

    trying to get isotope working with the search results

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,18 @@ $(document).ready(()=> {
 
     const nowPlayingUrl = apiBaseUrl + '/movie/now_playing?api_key=' + apiKey;
 
+    let buttonsHTML = '';
+    buttonsHTML += `<button class="all-button all">All Genres</button>`
+    for (i=0; i<genreArray.length; i++) {
+        buttonsHTML += `<button class="genre-button all">${genreArray[i].name}</button>`
+    }
+    $('#genre-buttons').html(buttonsHTML)
     // console.log(nowPlayingUrl)
     // Make AJAX request to nowPlayingUrl
     $.getJSON(nowPlayingUrl, (nowPlayingData)=> {
         // console.log(nowPlayingData)
-        let nowPlayingHTML = '';
-        for (let i=0; i < nowPlayingData.results.length; i++) {
-            let posterUrl = imageBaseUrl +'w300'+ nowPlayingData.results[i].poster_path;
-            nowPlayingHTML += `<div class="movie-poster" movie-id="${nowPlayingData.results[i].id}"><img src="${posterUrl}"></div>`;
-        }
+        let nowPlayingHTML = getHTML(nowPlayingData);
+        
         console.log(nowPlayingData);
         let nowPlayingTitle = `<h1>Now Playing:</h1>`
         $('#grid-header').html(nowPlayingTitle)
@@ -40,15 +43,28 @@ $(document).ready(()=> {
                 }
                 cast = castList.join(', ');
                 $.fancybox.open(`<div class="message"><h2>${modalTitle}</h2><h3>Starring:</h3><ul>${cast}</ul><h3>Synopsis:</h3>${modalHTML}</div>`);
-                })
+                });
                 // open the modal
                 
-            })
+            });
             
             
+        });
+        // console.log($('#movie-grid'))
+        $grid = $('#movie-grid').isotope({
+            itemSelector: '.movie-poster',
+    
+        });
+        // $grid.imagesLoaded().progress( function() {
+        //     $grid.isotope('layout');
+        // });
+        $('.genre-button').click(function(){
+            $grid.isotope({ filter: '.'+this.innerText })
         })
-        console.log($('#movie-grid'))
-    })
+        $('.all-button').click(function() {
+            $grid.isotope({ filter: '.all' });
+        });
+    });
 
     $('#movie-form').submit((event)=> {
         // Don't submit form! Js will handle
@@ -63,7 +79,17 @@ $(document).ready(()=> {
             $('#movie-grid').html(searchMovieHTML);
             console.log(searchMovieData)
 
+            $grid = $('#movie-grid').isotope({
+            itemSelector: '.movie-poster',
+        });
+        console.log($grid)
             
+            $('.genre-button').click(function(){
+                $grid.isotope({ filter: '.'+this.innerText })
+            })
+            $('.all-button').click(function() {
+                $grid.isotope({ filter: '.all' });
+            });
             
             $('.movie-poster').click((event)=> {
             console.dir(event.target)
@@ -95,17 +121,32 @@ $(document).ready(()=> {
             
         })
     
+        // console.log($('#movie-grid'))
+        
+        
 
-        function getHTML(data) {
+    })    
+    function getHTML(data) {
         let newHTML = '';
         for (let i=0; i < data.results.length; i++) {
+            // set up a var for the genre ids for THIS movie
+            let thisMovieGenres = data.results[i].genre_ids;
+            let movieGenreClassList = " ";
+            console.log(data.results[i])
+            // loop through all genres
+            for (let j=0; j < genreArray.length; j++) {
+                // check to see if the genre we are on (genreArray[j]) is in THIS movie
+                if (thisMovieGenres.indexOf(genreArray[j].id) > -1) {
+                    // HIT! this genre id is in THIS movie's genre id list so we need to add the name to the class list
+                    movieGenreClassList += genreArray[j].name + " ";
+                }
+                
+            }
+            console.log(movieGenreClassList);
             let posterUrl = imageBaseUrl +'w300'+ data.results[i].poster_path;
-            newHTML += `<div class="movie-poster" movie-id="${data.results[i].id}"><img src="${posterUrl}"></div>`;
+            newHTML += `<div class="movie-poster ${movieGenreClassList} all" movie-id="${data.results[i].id}"><img src="${posterUrl}"></div>`;
         }
         return newHTML;
     }
-
-    })    
-    
 
 });
